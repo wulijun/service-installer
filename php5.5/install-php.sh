ed4b38f3272c974b7c8d2a988e5c5f0361a528ef
wulijun <wulijun01234@gmail.com>
2013-12-25 11:09:47 +0800

    saved

--- php5.5/install-php.sh
+++ php5.5/install-php.sh
@@ -10,13 +10,11 @@ make clean
 PATH=$PATH:$LIB_PATH/bin
 ./configure \
 --prefix=$INSTALL_PHP_PATH  \
-	--enable-dom \
-	--enable-opcache \
 	--with-jpeg-dir=$LIB_PATH  \
 	--with-png-dir=$LIB_PATH \
-	--with-gd=$LIB_PATH \
 	--with-freetype-dir=$LIB_PATH \
 	--with-zlib-dir=$LIB_PATH \
+	--with-gd=$LIB_PATH \
 	--with-mcrypt=$LIB_PATH \
 	--with-mysql=mysqlnd \
 	--with-pdo-mysql=mysqlnd \
@@ -25,7 +23,8 @@ PATH=$PATH:$LIB_PATH/bin
 	--with-iconv \
 	--enable-mbstring \
 	--with-xmlrpc \
-	--enable-safe-mode \
+    --enable-dom \
+    --enable-opcache \
 	--enable-sockets \
 	--enable-ftp \
 	--enable-shmop \
@@ -33,18 +32,15 @@ PATH=$PATH:$LIB_PATH/bin
 	--with-config-file-path=$PHP_CONF_PATH \
 	--enable-xml \
 	--with-libxml-dir=$LIB_PATH \
-	--with-pcre-dir=$LIB_PATH \
 	--with-curl=$LIB_PATH \
-	--with-curlwrappers \
 	--enable-fpm \
 	--without-pear \
-	--without-sqlite \
 	--without-sqlite3 \
 	--without-pdo-sqlite \
 	--enable-pcntl \
 	--enable-bcmath
 make && make install
-cp ./sapi/fpm/init.d.php-fpm ./sapi/fpm/status.html $INSTALL_PHP_PATH/sbin/
+cp ./sapi/fpm/init.d.php-fpm $INSTALL_PHP_PATH/sbin/
 chmod +x $INSTALL_PHP_PATH/sbin/init.d.php-fpm
 cp $SRC_ROOT_PATH/rotate_log.sh $INSTALL_PHP_PATH/sbin/
 chmod +x $INSTALL_PHP_PATH/sbin/rotate_log.sh
@@ -54,16 +50,15 @@ mkdir -p $INSTALL_PHP_PATH/data
 cp -r $DATA_ROOT_PATH/* $INSTALL_PHP_PATH/data/
 
 mkdir -p $INSTALL_PHP_PATH/lib/php-libs
-mkdir -p $INSTALL_PHP_PATH/lib/exts
-cp -r $SRC_ROOT_PATH/php-exts/* $INSTALL_PHP_PATH/lib/exts/
+cp -r $SRC_ROOT_PATH/php-exts/* $INSTALL_PHP_PATH/lib/php/extensions/no-debug-non-zts-20121212
 cp -r $SRC_ROOT_PATH/php-libs/* $INSTALL_PHP_PATH/lib/php-libs
 
 mkdir -p $INSTALL_PHP_PATH/etc
 cp -r $CONF_ROOT_PATH/* $INSTALL_PHP_PATH/etc/
-mv $INSTALL_PHP_PATH/etc/php.ini $INSTALL_PHP_PATH/php.ini
+#mv $INSTALL_PHP_PATH/etc/php.ini $PHP_CONF_PATH
 sed -e "s:/home/worker/php5:$INSTALL_PHP_PATH:g" -i $INSTALL_PHP_PATH/etc/php-fpm.conf
 sed -e "s:{CUR_USER}:$CUR_USER:g" -i $INSTALL_PHP_PATH/etc/php-fpm.conf
-sed -e "s:/home/worker/php5:$INSTALL_PHP_PATH:g" -i $INSTALL_PHP_PATH/php.ini
+sed -e "s:/home/worker/php5:$INSTALL_PHP_PATH:g" -i $PHP_CONF_PATH/php.ini
 
 #mkdir -p $HOME_PATH/app-php/webroot
 #cp -r $SRC_ROOT_PATH/../webroot/* $HOME_PATH/app-php/webroot/
