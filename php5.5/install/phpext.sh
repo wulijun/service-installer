4ed58e388a99d207dcd893c36903be0c0eec1563
风之缘 <wulijun01234@gmail.com>
2014-08-23 15:53:55 +0800

    Update phpext.sh

--- php5.5/install/phpext.sh
+++ php5.5/install/phpext.sh
@@ -4,17 +4,7 @@ source ./conf.sh
 
 cd $SRC_ROOT_PATH
 
-#EXTNAME=djutil-1.0.1
-#tar -xzvf $EXTNAME.tar.gz
-#cd $EXTNAME
-#$INSTALL_PHP_PATH/bin/phpize
-#./configure --with-php-config=$INSTALL_PHP_PATH/bin/php-config
-#make
-#cp modules/*.so $INSTALL_PHP_PATH/lib/php/extensions/no-debug-non-zts-20121212
-#cd ../
-#rm -rf $EXTNAME
-
-EXTNAME=phpredis-2.2.5
+EXTNAME=djutil-1.0.2
 tar -xzvf $EXTNAME.tar.gz
 cd $EXTNAME
 $INSTALL_PHP_PATH/bin/phpize
@@ -23,3 +13,14 @@ make
 cp modules/*.so $INSTALL_PHP_PATH/lib/php/extensions/no-debug-non-zts-20121212
 cd ../
 rm -rf $EXTNAME
+
+EXTNAME=mongo-1.5.5
+tar -xzvf $EXTNAME.tgz
+cd $EXTNAME
+$INSTALL_PHP_PATH/bin/phpize
+./configure --with-php-config=$INSTALL_PHP_PATH/bin/php-config
+make
+cp modules/*.so $INSTALL_PHP_PATH/lib/php/extensions/no-debug-non-zts-20121212
+cd ../
+rm -rf $EXTNAME
+
