a08d3c18c894b0864496b7da9e40e13393fbc4af
风之缘 <wulijun01234@gmail.com>
2014-10-24 15:10:11 +0800

    Update libxml.sh

--- php5.5/install/libxml.sh
+++ php5.5/install/libxml.sh
@@ -3,12 +3,12 @@
 . ./conf.sh
 
 cd $SRC_ROOT_PATH
-tar zxvf libxml2-2.9.1.tar.gz
-cd libxml2-2.9.1
+tar zxvf libxml2-2.9.2.tar.gz
+cd libxml2-2.9.2
 make clean
 ./configure --prefix=$LIB_PATH
 make
 make install
 
 cd ../
-rm -rf libxml2-2.9.1
+rm -rf libxml2-2.9.2
